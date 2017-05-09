a6c8f7c9d134bac673efb8fbacb4a0ff74e1b9cd
Bohdan Basov <basovbohdan@gmail.com>
2017-05-09 12:25:03 +0000

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+      res.send({ todos });
+   }, (err) => {
+      res.status(400).send(err);
+   });
+});
+
 app.post('/todos', (req, res) => {
    var newTodo = new Todo({
       text: req.body.text
