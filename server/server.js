22410c8eb2e050626eb35224f714a1c3a2d1e43c
Bohdan Basov <basovbohdan@gmail.com>
2017-05-14 10:30:30 +0000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var { ObjectId } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -8,7 +9,7 @@ var { User } = require("./models/user");
 var app = express();
 
 app.use(bodyParser.json());
-
+// GET ALL
 app.get('/todos', (req, res) => {
    Todo.find().then((todos) => {
       res.send({ todos });
@@ -16,7 +17,7 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
    });
 });
-
+// POST NEW
 app.post('/todos', (req, res) => {
    var newTodo = new Todo({
       text: req.body.text
@@ -27,6 +28,24 @@ app.post('/todos', (req, res) => {
    }, (err) => {
       res.status(400).send(err);
    });
+});
+// GET ONE
+app.get('/todos/:id', (req, res) => {
+   var id = req.params.id;
+   
+   if(!ObjectId.isValid(id)) {
+      return res.status(404).send();
+   }
+   
+   Todo.findById(id).then((todo) => {
+      if(!todo) {
+         return res.status(404).send();   
+      }
+      res.status(200).send({ todo });
+   }).catch((err) => {
+      res.status(400).send(err);
+   });
+   
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
