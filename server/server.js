fef34db2946ad0ef7dad15ddfbae18b9f652cc86
Bohdan Basov <basovbohdan@gmail.com>
2017-05-16 15:10:39 +0000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var { ObjectId } = require("mongodb");
+const { ObjectId } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
@@ -59,6 +60,33 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
    }
    Todo.findByIdAndRemove(id).then((todo) => {
+      if(!todo) {
+         return res.status(404).send();
+      }
+      res.status(200).send({ todo });
+   }).catch((err) => {
+      res.status(400).send(err);
+   });
+});
+//PATCH ONE
+app.patch("/todos/:id", (req, res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text', 'completed']);
+   if(!ObjectId.isValid(id)) {
+      return res.status(404).send();
+   }
+   if(_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+   } else {
+      body.completed = false;
+      body.completedAt = null;
+   }
+   Todo.findByIdAndUpdate(id, {
+      $set: body   
+   }, 
+   {
+      new: true   
+   }).then((todo) => {
       if(!todo) {
          return res.status(404).send();
       }
